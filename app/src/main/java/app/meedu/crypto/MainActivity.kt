573749cf42b1b0fe3c4f774d21a971b563d8863a
darwin-morocho <darwin.morocho@icloud.com>
2023-12-07 19:19:44 -0500

    init

---
+++ app/src/main/java/app/meedu/crypto/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.meedu.crypto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
